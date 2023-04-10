e686bc6f8730f298221c89601023f2cdd96572cf
Gustavo Gonçalves <gustavo.goncalves@doutoresdaweb.com.br>
2023-04-09 23:56:37 -0300

    acionamento por tecla

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function tocaSom (idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-    
+
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
@@ -14,4 +14,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     };
 
+    tecla.onkeydown = function (evento) {
+        if (evento.code === "Space" || evento.code === "Enter") {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
